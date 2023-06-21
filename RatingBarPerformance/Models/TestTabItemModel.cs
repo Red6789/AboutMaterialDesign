ab41807d423421f385008fe7963172a82e51183c
red <93982350Red@gmail.com>
2023-06-21 18:29:46 +0800

    RatingBarPerformance

---
+++ RatingBarPerformance/Models/TestTabItemModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using Prism.Mvvm;
+
+namespace RatingBarPerformance.Models;
+
+public class TestTabItemModel : BindableBase
+{
+    public TestTabItemModel(string title)
+    {
+        Title = title;
+    }
+
+    public string Title { get; private init; }
+
+    private bool _isSelected;
+    public bool IsSelected
+    {
+        set => SetProperty(ref _isSelected, value);
+        get => _isSelected;
+    }
+
+    public ObservableCollection<TestItemModel> ItemModels { get; } = new();
+}
---
+++ RatingBarPerformance/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Mvvm;
+using RatingBarPerformance.Models;
+using System.Collections.ObjectModel;
+
+namespace RatingBarPerformance.ViewModels;
+
+public class MainViewModel : BindableBase
+{
+    public MainViewModel()
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            var tabItem = new TestTabItemModel($"Header{i}");
+            for (int j = 0; j < 100; j++)
+            {
+                tabItem.ItemModels.Add(new TestItemModel());
+            }
+            TabModels1.Add(tabItem);
+        }
+
+        for (int i = 1; i <= 3; i++)
+        {
+            var tabItem = new TestTabItemModel($"Header{i}");
+            for (int j = 0; j < 100; j++)
+            {
+                tabItem.ItemModels.Add(new TestItemModel());
+            }
+            TabModels2.Add(tabItem);
+        }
+    }
+
+    public ObservableCollection<TestTabItemModel> TabModels1 { get; } = new();
+
+    public ObservableCollection<TestTabItemModel> TabModels2 { get; } = new();
+}
